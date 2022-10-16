938830fae6d8e6f5170c8ce36d9605d815ef4540
LITENS\deqiang.wang <deqiang.wang@litens.com>
2022-10-16 10:59:51 -0400

    modification

--- Environment.py
+++ Environment.py
@@ -1,11 +1,35 @@
 from enum import Enum
+import copy
 import random
+from collections import namedtuple
 
 class Orientation(Enum):
     North = 1
     South = 2
     West = 3
     East = 4
+
+    @property
+    def turn_left(self):
+        dict_turn_left = {
+            Orientation.North: Orientation.West,
+            Orientation.West: Orientation.South,
+            Orientation.South: Orientation.East,
+            Orientation.East: Orientation.North
+        }
+        new_orientation = dict_turn_left.get(self)
+        return new_orientation
+
+    @property
+    def turn_right(self):
+        dict_turn_right = {
+            Orientation.North: Orientation.East,
+            Orientation.East: Orientation.South,
+            Orientation.South: Orientation.West,
+            Orientation.West: Orientation.North
+        }
+        new_orientation = dict_turn_right.get(self)
+        return new_orientation
 
 class Action(Enum):
     Forward = 1
@@ -15,42 +39,46 @@ class Action(Enum):
     Grab = 5
     Climb = 6
 
-class Coords:
-    x: int
-    y: int
+class Coords(namedtuple('Coords', 'x y')):
+    pass
 
-    def __init__(self, x: int, y:int) -> None:
-        self.x = x
-        self.y = y
 
-    def isAdjacentTo(self, coor1) -> bool:
-        return (self.x == coor1.x and abs(coor1.y - self.y) == 1) \
-        or (self.y == coor1.y and abs(coor1.x - self.x) == 1)
+# class Coords:
+#     x: int
+#     y: int
 
-    def adjacentCells(self, gridWidth: int, gridHeight: int) -> list:
-        adCells = []
-        # left
-        if self.x > 1:
-            adCells.append(Coords(self.x - 1, self.y))
-        # right
-        if self.x < gridWidth:
-            adCells.append(Coords(self.x + 1, self.y))
-        # down
-        if self.y > 1:
-            adCells.append(Coords(self.x, self.y - 1))
-        # up
-        if self.y < gridHeight:
-            adCells.append(Coords(self.x, self.y + 1))
-        return adCells
+#     def __init__(self, x: int, y:int) -> None:
+#         self.x = x
+#         self.y = y
 
-    def isEqual(self, coor1) -> bool:
-        return (self.x == coor1.x) and (self.y == coor1.y)
+#     def isAdjacentTo(self, coor1) -> bool:
+#         return (self.x == coor1.x and abs(coor1.y - self.y) == 1) \
+#         or (self.y == coor1.y and abs(coor1.x - self.x) == 1)
 
-    def isInside(self, coors: list) -> bool:
-        coors_xy = []
-        for coor in coors:
-            coors_xy.append((coor.x, coor.y))
-        return (self.x, self.y) in set(coors_xy)
+#     def adjacentCells(self, gridWidth: int, gridHeight: int) -> list:
+#         adCells = []
+#         # left
+#         if self.x > 1:
+#             adCells.append(Coords(self.x - 1, self.y))
+#         # right
+#         if self.x < gridWidth:
+#             adCells.append(Coords(self.x + 1, self.y))
+#         # down
+#         if self.y > 1:
+#             adCells.append(Coords(self.x, self.y - 1))
+#         # up
+#         if self.y < gridHeight:
+#             adCells.append(Coords(self.x, self.y + 1))
+#         return adCells
+
+#     def isEqual(self, coor1) -> bool:
+#         return (self.x == coor1.x) and (self.y == coor1.y)
+
+#     def isInside(self, coors: list) -> bool:
+#         coors_xy = []
+#         for coor in coors:
+#             coors_xy.append((coor.x, coor.y))
+#         return (self.x, self.y) in set(coors_xy)
 
 class Percept:
     stench: bool
@@ -89,7 +117,7 @@ class AgentState:
     hasArrow: bool
     isAlive: bool
 
-    def  __init__(self, location:Coords, orientation:Orientation, hasGold: bool, hasArrow: bool, isAlive: bool):
+    def  __init__(self, location: Coords = Coords(1,1), orientation:Orientation = Orientation.East, hasGold: bool =False, hasArrow: bool = True, isAlive: bool = True):
         self.location = location
         self.orientation = orientation
         self.hasGold = hasGold
@@ -97,51 +125,44 @@ class AgentState:
         self.isAlive = isAlive
 
     def turnLeft(self):
-        if self.orientation == Orientation.North:
-            self.orientation = Orientation.West
-        elif self.orientation == Orientation.South:
-            self.orientation = Orientation.East
-        elif self.orientation == Orientation.West:
-            self.orientation = Orientation.South
-        elif self.orientation == Orientation.East:
-            self.orientation = Orientation.North
+        new_state = copy.deepcopy(self)
+        new_state.orientation = new_state.orientation.turn_left
+        return new_state
 
     def turnRight(self):
-        if self.orientation == Orientation.North:
-            self.orientation = Orientation.East
-        elif self.orientation == Orientation.South:
-            self.orientation = Orientation.West
-        elif self.orientation == Orientation.West:
-            self.orientation = Orientation.North
-        elif self.orientation == Orientation.East:
-            self.orientation = Orientation.South
+        new_state = copy.deepcopy(self)
+        new_state.orientation = new_state.orientation.turn_right
+        return new_state
 
     def forward(self, gridWidth: int, gridHeight: int):
         if self.orientation == Orientation.North:
-            self.location = Coords(self.location.x, min(gridHeight, self.location.y + 1))
+            new_loc = Coords(self.location.x, min(gridHeight, self.location.y + 1))
         elif self.orientation == Orientation.South:
-            self.location = Coords(self.location.x, max(1, self.location.y - 1))
+            new_loc = Coords(self.location.x, max(1, self.location.y - 1))
         elif self.orientation == Orientation.West:
-            self.location = Coords(max(self.location.x - 1, 1), self.location.y)
+            new_loc = Coords(max(self.location.x - 1, 1), self.location.y)
         elif self.orientation == Orientation.East:
-            self.location = Coords(min(self.location.x + 1, gridWidth), self.location.y)    
+            new_loc = Coords(min(self.location.x + 1, gridWidth), self.location.y)           
+        new_state = copy.deepcopy(self)
+        new_state.location = new_loc
+        return new_state
 
-    def useArrow(self):
-        self.hasArrow = False
+    # def useArrow(self):
+    #     self.hasArrow = False
 
-    def applyMoveAction(self, action: Action, gridWidth: int, gridHeight: int):
-        if action == Action.Forward:
-            self.forward(gridWidth, gridHeight)
-        elif action == Action.TurnLeft:
-            self.turnLeft()
-        elif action == Action.TurnRight:
-            self.turnRight()
+    # def applyMoveAction(self, action: Action, gridWidth: int, gridHeight: int):
+    #     if action == Action.Forward:
+    #         self.forward(gridWidth, gridHeight)
+    #     elif action == Action.TurnLeft:
+    #         self.turnLeft()
+    #     elif action == Action.TurnRight:
+    #         self.turnRight()
 
-    def applyAction(self, action: Action, gridWidth: int, gridHeight: int):
-        if action == Action.Shoot:
-            self.useArrow()
-        else:
-            self.applyMoveAction(action, gridWidth, gridHeight)
+    # def applyAction(self, action: Action, gridWidth: int, gridHeight: int):
+    #     if action == Action.Shoot:
+    #         self.useArrow()
+    #     else:
+    #         self.applyMoveAction(action, gridWidth, gridHeight)
 
     def show(self):
         print(f"location: {self.location}, orientation: {self.orientation}, hasGold: {self.hasGold}, hasArrow: {self.hasArrow}, isAlive: {self.isAlive}")
@@ -160,33 +181,34 @@ class Environment:
     goldLocation: Coords
     percept: Percept
 
-    def __init__(self, gridWidth: int, gridHeight: int, pitProb: int, allowClimbWithoutGold: bool) -> None:
-        self.gridWidth = gridWidth
-        self.gridHeight = gridHeight
-        self.pitProb = pitProb
-        self.allowClimbWithoutGold = allowClimbWithoutGold
-        self.agent = AgentState(location=Coords(1,1), orientation=Orientation.East, hasGold=False, hasArrow=True, isAlive=True)
-        self.pitLocations = self._get_pit_locations(gridWidth, gridHeight)
-        self.terminated = False
-        self.wumpusLocation = self._get_wumpus_location()
-        self.wumpusAlive = True
-        self.goldLocation = self._get_gold_location()
+    def __init__(self, grid_width, grid_height, pit_prob, allow_climb_without_gold, agent, pit_locations,
+                 terminated, wumpus_loc, wumpus_alive, gold_loc) -> None:
+        self.gridWidth = grid_width
+        self.gridHeight = grid_height
+        self.pitProb = pit_prob
+        self.allowClimbWithoutGold = allow_climb_without_gold
+        self.agent = agent
+        self.pitLocations = pit_locations
+        self.terminated = terminated
+        self.wumpusLocation = wumpus_loc
+        self.wumpusAlive = wumpus_alive
+        self.goldLocation = gold_loc
         self.percept = Percept()
 
     def _isPitAt(self, coords: Coords) -> bool:
-        return coords.isInside(self.pitLocations)
+        return coords in self.pitLocations
 
     def _isWumpusAt(self, coords: Coords) -> bool:
-        return coords.isEqual(self.wumpusLocation)
+        return coords == self.wumpusLocation
 
     def _isAgentAt(self, coords: Coords) -> bool:
-        return coords.isEqual(self.agent.location)
+        return coords == self.agent.location
 
     def _isGlitter(self) -> bool:
-        return self.goldLocation.isEqual(self.agent.location)
+        return self.goldLocation == self.agent.location
 
     def _isGoldAt(self, coords: Coords) -> bool:
-        return coords.isEqual(self.goldLocation)      
+        return coords == self.goldLocation    
 
     def _wumpusInLineOfFire(self) -> bool:
         if self.agent.orientation == Orientation.West:
@@ -201,7 +223,15 @@ class Environment:
     def _killAttemptSuccessful(self) -> bool:
         return self.agent.hasArrow and self.wumpusAlive and self._wumpusInLineOfFire()
 
-    def _isPitAdjacent(self, coords: Coords) -> bool:
+    def _adjacentCells(self, coords: Coords = Coords(1,1)) ->list:
+        return [
+            Coords(coords.x - 1, coords.y) if coords.x > 1 else None, # to left
+            Coords(coords.x + 1, coords.y) if coords.x < self.gridWidth else None, # to right
+            Coords(coords.x, coords.y - 1) if coords.y > 1 else None, # to down
+            Coords(coords.x, coords.y + 1) if coords.y < self.gridHeight else None # to up
+        ]
+
+    def _isPitAdjacent(self, coords: Coords = Coords(1, 1)) -> bool:
         for pit in self.pitLocations:
             if pit.isInside(coords.adjacentCells(self.gridWidth, self.gridHeight)):
                 return True
@@ -218,12 +248,12 @@ class Environment:
 
     def _get_random_location(self, gridWidth: int, gridHeight: int) -> Coords:
         """ _get_random_location: return a random location that is not the (1,1) square """
-        x = 1
-        y = 1
-        while (x == 1) and (y == 1):
-            x = random.randint(1, gridWidth)
-            y = random.randint(1, gridHeight)
-        return Coords(x, y)
+        locations = []
+        for x in range(1, gridWidth + 1):
+            for y in range(1, gridHeight + 1):
+                locations.append(Coords(x = x,y = y))
+        locations.remove(Coords(1,1))
+        return random.choice(locations)
 
     def _get_gold_location(self) -> Coords:
         """ _get_gold_location: return a random location not (1,1) for the gold's location """
@@ -240,7 +270,7 @@ class Environment:
             for y in range(1, gridHeight + 1):
                 if (x != 1) and (y != 1):
                     # Using the PIT_PROBABILITY, randomly determine if a pit will be at this location
-                    if (random.randint(0, 1000 - 1)) < (self.pitProb * 1000):
+                    if random.random() < self.pitProb:
                         locations.append(Coords(x, y))
         return locations
 
@@ -322,7 +352,7 @@ class Environment:
         return "".join(st)
 
     def gridSymbol(self, x:int, y:int) -> str:
-        wumpusSymbol = 'W' if self.wumpusAlive else 'w'
+        wumpusSymbol = 'W ' if self.wumpusAlive else 'w '
         if self._isAgentAt(Coords(x, y)):
             if self.agent.orientation == Orientation.West:
                 return "A<"
@@ -333,10 +363,10 @@ class Environment:
             if self.agent.orientation == Orientation.South:  
                 return "Av"                    
         elif self._isPitAt(Coords(x, y)):
-            return "P"
+            return "P "
         elif self._isGoldAt(Coords(x, y)):
-            return "G"
+            return "G "
         elif self._isWumpusAt(Coords(x, y)):
             return wumpusSymbol
         else:
-            return " "
+            return "  "
