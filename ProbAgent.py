b08e4467abba44b10e240d4f926a66c3fcb2039e
LITENS\deqiang.wang <deqiang.wang@litens.com>
2022-11-02 17:55:37 -0400

    Prob Agent

--- ProbAgent.py
+++ ProbAgent.py
@@ -11,7 +11,8 @@ class ProbAgent(BeelineAgent.BeelineAgent):
 	wumpusLocProb: 	WumpusLocationProb.WumpusLocationProb
 	pitLocProb: 	PitLocationProb.PitLocationProb
 	riskProb: 		np.ndarray
-	unexploredQueue: list
+	unexploredLocs: set
+	targetLoc:		Environment.Coords
 
 	def __init__(self, gridWidth = 4, gridHeight = 4, safeLocations = set(), goldLocation = None, shortestPath = [], beelineActionList = []):
 		super(ProbAgent, self).__init__(gridWidth, gridHeight, safeLocations, goldLocation, shortestPath, beelineActionList)
@@ -27,14 +28,16 @@ class ProbAgent(BeelineAgent.BeelineAgent):
 			for j in range(4):
 				self.riskProb[i][j] = 1 - (1 - 1/15)*(1 - 0.2)
 		self.riskProb[0][0] = 0		
-		# initialize queue
-		self.unexploredQueue = []
+		# initialize unexplored set
+		self.unexploredLocs = set()
 		for i in range(4):
 			for j in range(4):
 				if i != 0 or j != 0:
-					heappush(self.unexploredQueue, (np.linalg.norm((i-0, j-0)), Environment.Coords(i+1, j+1)))		
+					self.unexploredLocs.add(Environment.Coords(i+1, j+1))	
+		# initial target loc
+		self.targetLoc = Environment.Coords(1, 1)
 
-	def updateRisk(self, loc: Environment.Coords, stench:bool, breeze:bool):
+	def _updateRisk(self, loc: Environment.Coords, stench:bool, breeze:bool):
 		self.wumpusLocProb.updateWumpusProb(loc, stench)
 		self.pitLocProb.updatePitProb(loc, breeze)
 		for i in range(4):
@@ -43,21 +46,69 @@ class ProbAgent(BeelineAgent.BeelineAgent):
 					self.riskProb[i][j] = 1 - (1 - self.wumpusLocProb.wumpusProb[i][j])\
 						* (1 - self.pitLocProb.pitProb[i][j])		
 
-	def checkNextUnexplored(self):
-		for i in range(len(self.unexploredQueue)):
-			nextLoc = heappop(self.unexploredQueue)
-			if self.riskProb[nextLoc[1].x - 1][nextLoc[1].y - 1] < 0.5:
-				return nextLoc[1]
-		return None
+	def _bfs_shortestpath(self, agentLoc:Environment.Coords, destination:Environment.Coords) -> list:
+		path = []
+		explored = []
+		visited = []
+		path_dict = {}
+		desti = destination
+		start = agentLoc
+		explored.append(start)
+		curr = explored.pop(0)
+		visited.append(curr)
+		while curr != desti:
+			for coord in self._adjacent(curr, 4, 4):
+				if coord in self.safeLocations and (not coord in visited):
+					explored.append(coord)
+					path_dict.update({coord:curr})
+					visited.append(coord)
+			curr = explored.pop(0)
+            # if curr == desti:
+            #     path_dict.update({desti:curr})
+        
+		path.append(desti)
+		prev_path = path_dict.get(desti)
+		path.insert(0,prev_path)
+		while prev_path != start:
+			prev_path = path_dict.get(prev_path)
+			path.insert(0,prev_path)
+		return path
 
+	def _findNextLoc(self, agentLoc: Environment.Coords, unexploreQueue:list):
+		nextLoc = Environment.Coords(1, 1)
+		path = []
+		for i in range(len(unexploreQueue)):
+			x = unexploreQueue[i][1].x
+			y = unexploreQueue[i][1].y
+			if self.riskProb[x-1][y-1] < 0.5:
+				nextLoc = unexploreQueue[i][1]
+				path = self._bfs_shortestpath(agentLoc, nextLoc)
+				if path:
+					return nextLoc, path
+		return nextLoc, path
+			
 
 	def nextAction(self, agentState: Environment.AgentState, percept: Environment.Percept) -> Environment.Action:
-		nextLoc = Environment.Coords(0,0)
-		# Safe location if not terminated
+		# update risk
+		self._updateRisk(agentState.location, percept.stench, percept.breeze)
+
 		if not agentState.hasGold:
-			self.updateRisk(agentState.location, percept.stench, percept.breeze)
-			self.safeLocations.add(agentState.location)
-			nextLoc = self.checkNextUnexplored()
+			# 1. Plan for next target location
+			if agentState.location == self.targetLoc:
+				# 1.1 Update unexplored queue
+				unexploreQueue = []
+				for loc in self.unexploredLocs:
+					heappush(unexploreQueue, (np.linalg.norm((loc.x-1, loc.y-1)), loc))
+				# 1.2 Find target location
+				self.targetLoc, self.shortestPath = self._findNextLoc(agentState.location, unexploreQueue)
+			# 2. Go to target location
+			else:
+				if agentState.orientation != self._requireOrientation():
+					return self._turn(agentState.orientation, self._requireOrientation())
+				else:
+					self.shortestPath.pop(0)
+					return Environment.Action.Forward
+
 		# Climb when hasGold and at (1,1)
 		if agentState.hasGold and agentState.location == Environment.Coords(1,1):
 			return Environment.Action.Climb
@@ -73,16 +124,17 @@ class ProbAgent(BeelineAgent.BeelineAgent):
 			else:
 				self.shortestPath.pop(0)
 				return Environment.Action.Forward
+		return None
 		# Other random
-		randAction = random.randint(1, 4)
-		if randAction == 1:
-			return Environment.Action.Forward
-		if randAction == 2:
-			return Environment.Action.TurnLeft
-		if randAction == 3:
-			return Environment.Action.TurnRight
-		if randAction == 4:
-			return Environment.Action.Shoot                
+		# randAction = random.randint(1, 4)
+		# if randAction == 1:
+		# 	return Environment.Action.Forward
+		# if randAction == 2:
+		# 	return Environment.Action.TurnLeft
+		# if randAction == 3:
+		# 	return Environment.Action.TurnRight
+		# if randAction == 4:
+		# 	return Environment.Action.Shoot                
 
 
 if __name__ == '__main__':
---
+++ ProbAgent_WumpusWorld.py
@@ -0,0 +1,27 @@
+import Environment
+import ProbAgent
+
+def main():
+    agent = ProbAgent.ProbAgent()
+    env = Environment.Environment()
+    env = env.new_game(4, 4, 0.2, False)
+    print(f"Step 0: \n")
+    print(f"{env.visualize()}")
+    env.percept.show()
+    step = 0
+    total_award = env.percept.reward
+
+    print(f"{Environment.Coords(1,2).x}")
+    while not env.percept.isTerminated:
+        step = step + 1
+        nextAction = agent.nextAction(env.agent, env.percept)
+        env = env.applyAction(nextAction)
+        print(f"Step {step} Action: {nextAction}\n")
+        print(f"{env.visualize()}")
+        env.percept.show()
+        total_award = total_award + env.percept.reward
+        print(f"Total reward: {total_award}\n")
+
+
+if __name__ == '__main__':
+    main()
