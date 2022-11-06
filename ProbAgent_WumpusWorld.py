3dc9505082f9720ae6cbc439a39650c4ec3cea99
LITENS\deqiang.wang <deqiang.wang@litens.com>
2022-11-06 13:32:41 -0500

    fail most of time

--- ProbAgent_WumpusWorld.py
+++ ProbAgent_WumpusWorld.py
@@ -1,45 +1,49 @@
 import Environment
 import ProbAgent
 
-MAX_RUN = 10
+MAX_RUN = 1000
 
 def main():
     total_award_all = []
+    game = 0 
     for i in range(MAX_RUN):
+        game = game +1
+        print(game)
         agent = ProbAgent.ProbAgent()
         env = Environment.Environment()
         env = env.new_game(4, 4, 0.2, True)
-        print(f"Step 0: \n")
-        env.percept.show()
-        print(f"{env.visualize()}")
+        # print(f"Step 0: \n")
+        # env.percept.show()
+        # print(f"{env.visualize()}")
         nextAction = agent.nextAction(env.agent, env.percept)
-        print(f"nextAction: {nextAction} AgentLoc: ({env.agent.location.x}, {env.agent.location.y}) TargetLoc: ({agent.targetLoc.x}, {agent.targetLoc.y})\n")
-        print(f"RiskProb:\n{agent.visualizeRiskProb()}")
-        print(f"WumpusProb:\n{agent.visualizeWumpusProb()}")
-        print(f"PitProb:\n{agent.visualizePitProb()}")      
-        print(f"Unexplored:\n{agent.visualizeUnexplored()}") 
-        print(agent.shortestPath) 
+        # print(f"nextAction: {nextAction} AgentLoc: ({env.agent.location.x}, {env.agent.location.y}) TargetLoc: ({agent.targetLoc.x}, {agent.targetLoc.y})\n")
+        # print(f"RiskProb:\n{agent.visualizeRiskProb()}")
+        # print(f"WumpusProb:\n{agent.visualizeWumpusProb()}")
+        # print(f"PitProb:\n{agent.visualizePitProb()}")      
+        # print(f"Unexplored:\n{agent.visualizeUnexplored()}") 
+        # print(agent.shortestPath) 
 
         step = 0
         total_award = env.percept.reward
 
         while not env.percept.isTerminated:
             step = step + 1
-            print(f"Step {step}")
+            # print(f"Step {step}")
             env = env.applyAction(nextAction)
-            env.percept.show()
-            print(f"{env.visualize()}") 
+            # env.percept.show()
+            # print(f"{env.visualize()}") 
             nextAction = agent.nextAction(env.agent, env.percept)          
-            print(f"nextAction: {nextAction} AgentLoc: ({env.agent.location.x}, {env.agent.location.y}) TargetLoc: ({agent.targetLoc.x}, {agent.targetLoc.y})\n")
-            print(f"RiskProb:\n{agent.visualizeRiskProb()}")
-            print(f"WumpusProb:\n{agent.visualizeWumpusProb()}")
-            print(f"PitProb:\n{agent.visualizePitProb()}")        
-            print(f"Unexplored:\n{agent.visualizeUnexplored()}")
-            print(agent.shortestPath) 
+            # print(f"nextAction: {nextAction} AgentLoc: ({env.agent.location.x}, {env.agent.location.y}) TargetLoc: ({agent.targetLoc.x}, {agent.targetLoc.y})\n")
+            # print(f"RiskProb:\n{agent.visualizeRiskProb()}")
+            # print(f"WumpusProb:\n{agent.visualizeWumpusProb()}")
+            # print(f"PitProb:\n{agent.visualizePitProb()}")        
+            # print(f"Unexplored:\n{agent.visualizeUnexplored()}")
+            # print(agent.shortestPath) 
             total_award = total_award + env.percept.reward
-            print(f"Total reward: {total_award}\n")
+            # print(f"Total reward: {total_award}\n")
         total_award_all.append(total_award)
     print(f"Total reward: {total_award_all}\n")
+    print(f"Total reward: {sum(total_award_all)}\n")
 
 
 if __name__ == '__main__':
